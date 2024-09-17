bdd88dfc8107a99230a5b64101b6575b9553f90d
Sahas_Tech <sahasnethsara2010@gmail.com>
2024-09-17 16:26:33 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put sesson id",
+SESSION_ID: process.env.SESSION_ID || "TAkiRATC#QRBYKXUyIN-SmIeT32UVFJEAabsFINcyOVATHsMeAh0",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/tCkmbMx/IMG-20240913-WA0041.jpg",
 ALIVE_MSG: process.env.ALIVE_IMG || "🤖🔰 Hi QUEEN-KYLIE-MD Is Online Now 💻\n*💻 Owner* - QUEEN-KYLIE-MD\n\n*💻 Owner Number* -94718913389",
 SUDO_NB: process.env.SUDO_NB || "94718913389",
